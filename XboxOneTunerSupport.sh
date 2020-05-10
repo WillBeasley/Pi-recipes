3ca0e6a55fc17c8604a244c615818d5371893ee2
William Beasley <will.beasley@outlook.com>
2020-05-10 22:46:53 +0100

    Update XboxOneTunerSupport.sh

--- XboxOneTunerSupport.sh
+++ XboxOneTunerSupport.sh
@@ -1,7 +1,8 @@
+cd ~
 
 #Need to get the latest firmware because pi probably doesn't have it
-
-
+wget http://palosaari.fi/linux/v4l-dvb/firmware/MN88472/02/latest/dvb-demod-mn88472-02.fw
+sudo mv dvb-demod-mn88472-02.fw /lib/firmware
 
 
 #Need to modify this otherwise they wont show up in plex due to permissions problems.
