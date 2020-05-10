40c234a0e8f52a06e1296999c2eacad1da6bccb8
William Beasley <will.beasley@outlook.com>
2020-05-10 22:43:46 +0100

    Create XboxOneTunerSupport.sh

---
+++ XboxOneTunerSupport.sh
@@ -0,0 +1,8 @@
+
+#Need to get the latest firmware because pi probably doesn't have it
+
+
+
+
+#Need to modify this otherwise they wont show up in plex due to permissions problems.
+sudo chmod -R 777 /dev/dvb
